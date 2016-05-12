550a1babcb94a2b1d77daab36bbbcd3a514e1b6f
Melissa Canales <missmeliss621@gmail.com>
2016-05-12 10:32:00 -0500

    removed extra files

--- db_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define ('DB_HOST','mysql:host=127.0.0.1;');
-define ('DB_NAME','dbname=employees');
-define ('DB_USER' ,'codeup');
-define ('DB_PASS', 'Parker123');
-// constants dont get $.
-//semicolon only use between host and name to concatenate them.
